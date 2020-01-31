88b5150f2a6fdcb8ade71818af951f8da95ab268
Homeira Azari <homeiraazari@gmail.com>
2020-01-31 17:08:55 -0500

    update panda route

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
+import wikipedia
 
 app = Flask(__name__)
 
@@ -13,10 +15,20 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
-@app.route('/bob')
+@app.route('/Homeira')
 def bob():
-    val = {"value": "bob"}
+    val = {"value": "Homeira"}
     return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+    result = wikipedia.summary(company, sentences=10)
+    return result
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
